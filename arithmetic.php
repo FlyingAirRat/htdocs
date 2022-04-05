be31d29481a80c1ef8d84b84e1355922c8469241
AirRat <airedmc@gmail.com>
2022-04-05 13:04:31 +0900

    add php examples

---
+++ arithmetic.php
@@ -0,0 +1,14 @@
+<?php
+    $n1 = 8;
+    $n2 = 2;
+    $result = $n1 + $n2;
+    print "$n1 + $n2 = $result <br>";
+    $result = $n1 - $n2;
+    print "$n1 - $n2 = $result <br>";
+    $result = $n1 * $n2;
+    print "$n1 * $n2 = $result <br>";
+    $result = $n1 / $n2;
+    print "$n1 / $n2 = $result <br>";
+    $result = $n1 % $n2;
+    print "$n1 % $n2 = $result <br>";
+?>
---
+++ student.php
@@ -0,0 +1,6 @@
+<?php
+    $age = 21;
+    $blood_type = 'O';
+    $name = '장보고';
+    
+?>
