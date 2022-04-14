d1448a49d3afdf0f7c38103b1ef3a48cfe9dc3a1
AirRat <airedmc@gmail.com>
2022-04-14 14:33:00 +0900

    add date example

---
+++ ch07/getdate.php
@@ -0,0 +1,6 @@
+<?php
+    $seoul = getdate();
+    print "현재시간 : ".$seoul["year"]."년 ".$seoul["mon"]."월 ".
+    $seoul["mday"]."일 ".$seoul["hours"]."시 ".
+    $seoul["minutes"]."분 ".$seoul["seconds"]."초 ";
+?>
