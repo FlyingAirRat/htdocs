c889c223b8efc96bf49568376d8527fa7354ee73
AirRat <airedmc@gmail.com>
2022-04-14 14:58:31 +0900

    add bulletin board example

---
+++ ch07/write_proc.php
@@ -0,0 +1,15 @@
+<?php
+    include "../db/connect.php";
+    $title = $_POST["title"];
+    $ctnt = $_POST["ctnt"];
+    print "title: $title<br>";
+    print "ctnt : $ctnt";
+
+    $conn = get_conn();
+    $sql=
+    "INSERT INTO t_board(title,ctnt)
+    VALUES('${title}', '${ctnt}')
+    ";
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
+?>
--- db/connect.php
+++ db/connect.php
@@ -1,11 +1,9 @@
 <?php
-    define("URL", "localhost");
-    define("USERNAME", "root");
-    define("PASSWORD", "506greendg@");
-    define("DB_NAME", "board1");
-    $conn = mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME, "3306");
-    $sql = "INSERT INTO t_board(title, ctnt)
-    VALUE('test', 'content')";
-    mysqli_query($conn, $sql);
-    mysqli_close($conn);
+    function get_conn(){
+        define("URL", "localhost");
+        define("USERNAME", "root");
+        define("PASSWORD", "506greendg@");
+        define("DB_NAME", "board1");
+        return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME);
+    };
 ?>
