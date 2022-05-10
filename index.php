224fdbb57559d1cf6f622a47e14d08b497d3ba61
AirRat <airedmc@gmail.com>
2022-05-10 09:34:25 +0900

    add camera test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Airrat's Lair</title>
+</head>
+<body>
+<h1>Airrat's webserver</h1>
+<p>2022-05-04 HTTPS Applied.</p>
+<p>Under construction Right now...</p>
+카메라 테스트 : <a href="https://airrat.xyz/src/content/peerconnection/constraints/"><button>여기</button></a>
+</body>
+</html>
